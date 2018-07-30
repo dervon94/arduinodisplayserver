acf272cbfe4a36fa33f7bda1c7c8b89b0fcab3fd
dervon94 <dervon94@gmail.com>
2018-07-30 12:09:27 +0200

    Initialize Commit

---
+++ src/main/java/hu/miskolc/uni/iit/arduinodisplayserver/ArduinodisplayserverApplication.java
@@ -0,0 +1,12 @@
+package hu.miskolc.uni.iit.arduinodisplayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArduinodisplayserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArduinodisplayserverApplication.class, args);
+    }
+}
