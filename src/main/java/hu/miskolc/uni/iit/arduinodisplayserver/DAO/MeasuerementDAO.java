303f95d5788050619dbfcf3133574443009880ca
dervon94 <dervon94@gmail.com>
2018-07-30 13:15:28 +0200

    MeasurementDAO and  Display Service interfaces added.

--- src/main/java/hu/miskolc/uni/iit/arduinodisplayserver/DAO/MeasuerementDAO.java
+++ src/main/java/hu/miskolc/uni/iit/arduinodisplayserver/DAO/MeasuerementDAO.java
@@ -1,6 +1,7 @@
 package hu.miskolc.uni.iit.arduinodisplayserver.DAO;
 
 import hu.miskolc.uni.iit.arduinodisplayserver.model.MeasuringDevice;
+import hu.miskolc.uni.iit.arduinodisplayserver.model.SensorValues;
 
 import java.util.List;
 
@@ -10,4 +11,6 @@ public interface MeasuerementDAO {
     List<MeasuringDevice> listAllMeasurementWithDevices();
 
     List<MeasuringDevice> listAllMeasurement();
+
+    List<SensorValues> listSensorValuesByIpAdress(String ipAdress);
 }
